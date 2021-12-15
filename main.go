07768be1b3cff3ee8460c6208cc2220d43062475
Sumit Agrawal <sumit.agrawal@go-jek.com>
2021-12-15 19:00:09 +0530

    seperating out the buk buk package

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import "fmt"
 
-func RandomBukBuk() {
-	fmt.Println("Hey there, I am in RandomBukBuk func of your remote lib, here is your random buk buk --> in the news go is awarded as the best programming language in the universe, hence I am writing this random buk buk function in go")
-}
 
-//func main() {
-//	fmt.Println("Hey there, I am in main func of your remote lib")
-//	RandomBukBuk()
-//}
+
+func main() {
+	fmt.Println("Hey there, I am in main func of your remote lib")
+	RandomBukBuk()
+}
---
+++ randombukbuk/randombukbuk.go
@@ -0,0 +1,7 @@
+package randombukbuk
+
+import "fmt"
+
+func RandomBukBuk() {
+	fmt.Println("Hey there, I am in RandomBukBuk func of your remote lib, here is your random buk buk --> in the news go is awarded as the best programming language in the universe, hence I am writing this random buk buk function in go")
+}
